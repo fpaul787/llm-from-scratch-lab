bf455f1f50f106c8631c06fc646cb822054b8243
Frantz Paul <fpaul787@gmail.com>
2025-07-12 17:41:57 -0400

    gpt lab file

---
+++ llm_scratch_lab/lab.py
@@ -0,0 +1,20 @@
+
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,     # Vocabulary size
+    "context_length": 1024,  # Context length
+    "emb_dim": 768,          # Embedding dimension
+    "n_heads": 12,           # Number of attention heads
+    "n_layers": 12,          # Number of layers
+    "drop_rate": 0.1,        # Dropout rate
+    "qkv_bias": False        # Query-Key-Value bias
+}
+
+class GPTLab:
+    """A class representing a GPT Lab environment."""
+
+    def __init__(self):
+        self.config = GPT_CONFIG_124M
+
+    def run(self):
+        print("Running GPT Lab...")
