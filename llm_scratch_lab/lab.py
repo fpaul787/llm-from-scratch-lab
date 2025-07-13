f20bc8d0dec8b52272ffc91cc81e0f313e1382b3
Frantz Paul <fpaul787@gmail.com>
2025-07-12 21:23:23 -0400

    latest lab

--- llm_scratch_lab/lab.py
+++ llm_scratch_lab/lab.py
@@ -1,5 +1,7 @@
+from llm_scratch_lab.tokenizer import Tokenizer
+import torch
 
-
+# Define the configuration for the GPT model
 GPT_CONFIG_124M = {
     "vocab_size": 50257,     # Vocabulary size
     "context_length": 1024,  # Context length
@@ -15,6 +17,19 @@ class GPTLab:
 
     def __init__(self):
         self.config = GPT_CONFIG_124M
+        self.tokenizer = Tokenizer(encoding_name="gpt2")
+    
+    def __tensorize_tokens__(self, tokens, with_batch_dim=True):
+        """Converts tokens to a tensor format."""
+        tensor = torch.tensor(tokens, dtype=torch.long)
+        if with_batch_dim:
+            tensor = tensor.unsqueeze(0)  # Add batch dimension
+        return tensor
 
-    def run(self):
+    def run(self, text):
         print("Running GPT Lab...")
+        print("Tokenizing and tensorizing input text...")
+        # Tokenize the input text
+        tokens = self.tokenizer.tokenize(text)
+        # Tensorize the tokens
+        token_tensor = self.__tensorize_tokens__(tokens)
