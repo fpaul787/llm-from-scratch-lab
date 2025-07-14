f43fbcbacd6c09f915aac79751b0c3aadb27f9c1
Frantz Paul <fpaul787@gmail.com>
2025-07-13 20:40:42 -0400

    forward pass for GPTModel

--- llm_scratch_lab/models/gpt.py
+++ llm_scratch_lab/models/gpt.py
@@ -1,14 +1,30 @@
-
+import torch
 import torch.nn as nn
 
 class GPTModel(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        # self.__token_embedding__ = nn.Embedding(None, None) # Placeholder for token embedding layer
-        # Positional embedding
+        self.__token_embedding__ = nn.Embedding(self.config["vocabulary_size"], self.config["embedding_dimension"]) # Token embedding layer
+        self.__position_embedding__ = nn.Embedding(self.config["context_length"], self.config["embedding_dimension"]) # Position embedding layer
         # Dropout is optional
     
-    def forward(self):
-        # Placeholder for forward pass logic
+    def forward(self, input_tensor):
+        """
+        Forward pass for the GPT model.
+
+        Args:
+            input_tensor (torch.Tensor): Input tensor of shape (batch_size, seq_length) containing token indices.
+        Returns:
+            TBD
+        """
+        batch_size, seq_length = input_tensor.shape
+
+        # Retrieves the meanings of the input tokens (words)
+        token_embeddings = self.__token_embedding__(input_tensor) # Token embedding lookup.
+
+        # Retrieves the positions of the input tokens
+        position_embeddings = self.__position_embedding__(torch.arange(seq_length, device=input_tensor.device)) # Position embedding lookup
+        
+        x = token_embeddings + position_embeddings # Combine token and position embeddings
         pass
