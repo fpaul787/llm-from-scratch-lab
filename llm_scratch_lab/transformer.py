70b7ed67b0f3da06d832ea021ae2820af7280856
Frantz Paul <fpaul787@gmail.com>
2025-07-16 00:23:38 -0400

    using multiheadattention in transformer block

--- llm_scratch_lab/transformer.py
+++ llm_scratch_lab/transformer.py
@@ -1,9 +1,16 @@
 import torch.nn as nn
 
+from llm_scratch_lab.attention import MultiHeadAttention
+
 class TransformerBlock(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.attention = None
+        self.attention = MultiHeadAttention(
+            d_input=config["embedding_dimension"],
+            d_output=config["embedding_dimension"],
+            num_heads=config["num_heads"],
+            context_length=config["context_length"]
+        )
 
     def forward(self, x):
         pass
