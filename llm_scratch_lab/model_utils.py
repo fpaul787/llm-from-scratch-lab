2011d95d16f5d05ba3b87356ebc7de1521208363
Frantz Paul <fpaul787@gmail.com>
2025-07-16 23:27:44 -0400

    adding GELU and FeedFoward

--- llm_scratch_lab/model_utils.py
+++ llm_scratch_lab/model_utils.py
@@ -14,3 +14,24 @@ class LayerNormalization(nn.Module):
         var = x.var(dim=-1, keepdim=True, unbiased=False)
         normalized_x = (x - mean) / torch.sqrt(var + self.eps)
         return self.weight * normalized_x + self.bias
+    
+
+class GELU(nn.Module):
+    """Gaussian Error Linear Unit activation function."""
+    def forward(self, x):
+        """Applies the GELU activation function."""
+        return 0.5 * x * (1 + torch.tanh(torch.sqrt(torch.tensor(2.0 / torch.pi)) * (x + 0.044715 * torch.pow(x, 3))))
+    
+
+class FeedForward(nn.Module):
+    """Feed-forward network with GELU activation."""
+    def __init__(self, config):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(config["embedding_dimension"], 4 * config["embedding_dimension"]),
+            GELU(),
+            nn.Linear(4 * config["embedding_dimension"], config["embedding_dimension"])
+        )
+
+    def forward(self, x):
+        return self.layers(x)
