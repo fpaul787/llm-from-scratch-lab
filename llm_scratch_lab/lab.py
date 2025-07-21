74ba9c39493b22d72245468c5787af491a1035cf
Frantz Paul <fpaul787@gmail.com>
2025-07-20 22:14:33 -0400

    refactoring

--- llm_scratch_lab/lab.py
+++ llm_scratch_lab/lab.py
@@ -5,13 +5,12 @@ import torch
 
 # Define the configuration for the GPT model
 GPT_CONFIG_124M = {
-    "vocabulary_size": 50257,     # Vocabulary size
-    "context_length": 1024,  # Context length
-    "embedding_dimension": 768,          # Embedding dimension
-    "num_heads": 12,           # Number of attention heads
-    "num_layers": 12,          # Number of layers
-    "drop_rate": 0.1,        # Dropout rate
-    "qkv_bias": False        # Query-Key-Value bias
+    "vocabulary_size": 50257,       # Vocabulary size
+    "context_length": 1024,         # Context length
+    "embedding_dimension": 768,     # Embedding dimension
+    "num_heads": 12,                # Number of attention heads
+    "num_layers": 12,               # Number of layers
+    "qkv_bias": False               # Query-Key-Value bias
 }
 
 class GPTLab:
@@ -31,18 +30,21 @@ class GPTLab:
             tensor = tensor.unsqueeze(0)  # Add batch dimension
         return tensor
 
-    def run(self, text):
-        print("Running GPT Lab...")
-        print("Tokenizing the input text...")
-        # Tokenize the input text
+    def run(self, text, with_manual_seed=False):
+        """Runs the GPT model with the provided text input."""
+        if with_manual_seed:
+            torch.manual_seed(123)
+            print("Manual seed set to 123 for reproducibility.")
+
+        print("Tokenizing the input text: ", text)
         self.__tokens__ = self.tokenizer.tokenize(text)
+        print("Tokenized Tokens:", self.__tokens__)
 
-        print("Converting tokens to PyTorch tensor...")
-        # Tensorize the tokens
+        print("\nConverting tokens to PyTorch tensor...")
         self.__token_tensor__ = self.__tensorize_tokens__(self.__tokens__)
-        print("Tokens:", self.__token_tensor__)
+        print("Tensor Tokens:", self.__token_tensor__)
 
-        print("Running the GPT model...")
+        print("\nRunning the GPT model...")
         
         max_tokens = 10
         for _ in range(max_tokens):
@@ -58,4 +60,5 @@ class GPTLab:
             self.__token_tensor__ = torch.cat((self.__token_tensor__, next_token), dim=1)
 
         decoded_text = self.tokenizer.detokenize(self.__token_tensor__.squeeze(0).tolist())
+        print("\nGenerated Output Tokens:", self.__token_tensor__)
         print("Decoded text:", decoded_text)
